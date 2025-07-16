07795d4b73299b2e23a085abbc93bd8f9e9268b7
TimShinners <140019254+TimShinners@users.noreply.github.com>
2025-07-16 09:14:16 +0200

    removed dependencies

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'torch',
-        'mcbo'
+        #'numpy',
+        #'torch',
+        #'mcbo'
     ],
     author='Timothy Shinners',
     description='PFNs For Mixed-Variable Bayesian allows for the use of PFNs as a surrogate function in Bayesian optimization methods',
